ec54f54239ec6deaded2f0bfcff0f76f7094132e
灰都 <2565465028@qq.com>
2022-09-12 12:29:00 +0800

    update copy msg func
    It could copy image message.

--- xuanbot/plugins/copy/__init__.py
+++ xuanbot/plugins/copy/__init__.py
@@ -1,9 +1,10 @@
-from nonebot import get_driver
 
-from .config import Config
+import random
+from nonebot.adapters.onebot.v11 import GroupMessageEvent
+from nonebot.permission import SUPERUSER
 
-global_config = get_driver().config
-config = Config.parse_obj(global_config)
+from nonebot import on_message
+from nonebot.matcher import Matcher
 
 # Export something for other plugin
 # export = nonebot.export()
@@ -13,3 +14,10 @@ config = Config.parse_obj(global_config)
 # def some_function():
 #     pass
 
+copy_msg = on_message(priority=5,permission=SUPERUSER)
+
+@copy_msg.handle()
+async def copy_func(matcher:Matcher,event:GroupMessageEvent):
+    result = random.uniform(0,200)
+    if  result<=10 :
+        await matcher.finish(event.get_message())
